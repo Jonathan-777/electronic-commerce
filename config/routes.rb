475260ad71b1b92c14980d9224d5bb6df934a4b1
Jonathan-777 <126800463+Jonathan-777@users.noreply.github.com>
2025-03-31 19:02:24 +0000

    feat: Add custom session controller and update routes for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   # devise_for :users   ## adds functionality
-  
+
     # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
     devise_for :users, path: 'auth', controllers: {
       sessions: 'auth/sessions',
       registrations: 'auth/registrations'
     }
+
+    # Rails.application.routes.draw do
+    #   devise_for :users, controllers: {
+    #     sessions: 'users/sessions'
+    #   }
+    # end
 
     get "home" => "home#index"
     get "layouts" => "layouts#index"
@@ -13,6 +19,8 @@ Rails.application.routes.draw do
 
     # Define authentication routes
     get "login" => "users#new"  # Login page (signup form)
+   
+
     post "login" => "users#create" # Handles user creation
     delete "logout" => "users#destroy" # Logout action
 
@@ -25,7 +33,14 @@ Rails.application.routes.draw do
         resources :user_data
       end
     end
-  
+
+
+    namespace :admin do
+      resources :users do
+        resources :user_data
+      end
+    end
+
     # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
     # Can be used by load balancers and uptime monitors to verify that the app is live.
     get "up" => "rails/health#show", as: :rails_health_check
@@ -36,7 +51,6 @@ Rails.application.routes.draw do
 
     # Defines the root path route ("/")
     # root "posts#index"w"
-  
 
 
 end
